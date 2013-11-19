7a026c618c38b3dd77b1af0d575627ea92a544b5
Christoph Giesel <mail@cgiesel.de>
2013-11-19 17:45:49 +0100

    uebungsblatt 5: masteraufgabe ftw

--- uebung5/aufgabe6.py
+++ uebung5/aufgabe6.py
@@ -1,16 +1,35 @@
 import nltk
 
 grammar = nltk.parse_cfg("""
-S -> N
-N ->  'a' N 'a' | 'b' N 'b' | 'a' | 'b' | L
-L ->
+S -> S G | G S
+S -> '1' | '3' | '5' | '7' | '9'
+G -> S S
+G -> '0' | '2' | '4' | '6' | '8'
 """)
 
 parser = nltk.ChartParser(grammar)
 
 words = [
-    ""
+    ("0", False),
+    ("1", True),
+    ("2", False),
+    ("3", True),
+    ("10", True),
+    ("100", True),
+    ("20", False),
+    ("200", False),
+    ("11", False),
+    ("111", True),
+    ("22", False),
+    ("212", True),
+    ("202", False),
+    ("2012", True),
+    ("02012", True),
+    ("000141111161110", True)
 ]
 
-for word in words:
-    print("{}: {}".format(word, parser.nbest_parse(word)))
+print('word: expected / matched')
+
+for (word, expected) in words:
+    matched = len(parser.nbest_parse(word)) != 0
+    print("{}: {} / {}".format(word, expected, matched))
