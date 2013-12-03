c8fcb8ad54ac3a74a661b9613d4d03974bca52d1
Christoph Giesel <mail@cgiesel.de>
2013-12-03 15:12:39 +0100

    ansatz aufgabe 2 übungsblatt 7

---
+++ uebung7/aufgabe2.py
@@ -0,0 +1,18 @@
+g = nltk.parse_cfg("""
+S -> 'null' | ZIFFER1 | Z | H | T
+Z -> ZIFFER2 UND ZEHNER | ZEHNER
+H -> ZIFFER2 HUNDERT | ZIFFER2 HUNDERT Z | ZIFFER2 HUNDERT ZIFFER1
+T -> T2 TAUSEND | T2 TAUSEND H | T2 TAUSEND Z | T2 TAUSEND ZIFFER1
+T2 -> ZIFFER2 | Z | H
+M -> M2 MILLION | M2 MILLION T | M2 MILLION H | M2 MILLION Z | M2 MILLION ZIFFER1
+M2 -> ZIFFER3 | Z | H
+ZIFFER -> 'zwei' | 'drei' | 'vier' | 'fünf' | 'sechs' | 'sieben' | 'acht' | 'neun'
+ZIFFER1 -> 'eins' | ZIFFER
+ZIFFER2 -> 'ein' | ZIFFER
+ZIFFER3 -> 'eine' | ZIFFER
+ZEHNER -> 'zehn' | 'zwanzig' | 'dreißig' | 'vierzig' | 'fünfzig' | 'sechzig' | 'siebzig' | 'achtzig' | 'neunzig'
+UND -> 'und'
+HUNDERT -> 'hundert'
+TAUSEND -> 'tausend'
+MILLION -> 'million'
+""")
