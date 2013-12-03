3e1cc5187955778039f59bac53f327b1f5a8e679
Christoph Giesel <mail@cgiesel.de>
2013-12-03 15:21:03 +0100

    aufgabe 2 fixes

--- uebung7/aufgabe2.py
+++ uebung7/aufgabe2.py
@@ -1,6 +1,6 @@
 g = nltk.parse_cfg("""
-S -> 'null' | ZIFFER1 | Z | H | T
-Z -> ZIFFER2 UND ZEHNER | ZEHNER
+S -> 'null' | ZIFFER1 | Z | H | T | M
+Z -> ZIFFER2 UND ZEHNER | ZEHNER | 'elf' | 'zwölf' | 'dreizehn' | 'vierzehn' | 'fünfzehn' | 'sechzehn' | 'siebzehn' | 'achtzehn' | 'neunzehn'
 H -> ZIFFER2 HUNDERT | ZIFFER2 HUNDERT Z | ZIFFER2 HUNDERT ZIFFER1
 T -> T2 TAUSEND | T2 TAUSEND H | T2 TAUSEND Z | T2 TAUSEND ZIFFER1
 T2 -> ZIFFER2 | Z | H
@@ -14,5 +14,5 @@ ZEHNER -> 'zehn' | 'zwanzig' | 'dreißig' | 'vierzig' | 'fünfzig' | 'sechzig' |
 UND -> 'und'
 HUNDERT -> 'hundert'
 TAUSEND -> 'tausend'
-MILLION -> 'million'
+MILLION -> 'million' | 'millionen'
 """)
